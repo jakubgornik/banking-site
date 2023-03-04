04106294cce3e02c82ed2a731554c555de73498b
revinto <jakub.gornikk@gmail.com>
2023-03-04 20:48:37 +0100

    Dynamic navigation for desktop and mobile

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,16 +7,17 @@ const Home = () => {
     <>
       <div className="bg-[radial-gradient(120%_190%_at_90%_15%,#1D2B41_0%,#020509_57.38%,#0F1A29_88.16%)]">
         <Navbar />
-        <div className="container relative mx-auto flex h-screen items-center justify-center ">
-          {/* <div className="relative z-10 h-1/2 w-1/5 rounded-lg border-2 border-white border-opacity-5 bg-white bg-opacity-5 shadow-2xl"></div> */}
-          {/* <Image
+        {/* <div className="container relative mx-auto flex h-screen items-center justify-center ">
+          <div className="relative z-10 h-1/2 w-1/5 rounded-lg border-2 border-white border-opacity-5 bg-white bg-opacity-5 shadow-2xl"></div>
+          <Image
             className="absolute"
             src={team}
             alt="Team of employees"
             width={500}
             height={500}
-          ></Image> */}
-        </div>
+          ></Image>
+        </div> */}
+        <div className="h-[5000px]"></div>
       </div>
     </>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,23 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        mint: "#39E8EA",
+      },
+      fontFamily: {
+        sans: '"Roboto", sans-serif',
+      },
+    },
+    screens: {
+      mobile: "320px",
+      "mobile-lg": "400px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
   },
   plugins: [],
 };
