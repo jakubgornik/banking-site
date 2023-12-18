2e117a6d83f4122d7ebe441c0a6e53b54969deb4
revinto <jakub.gornikk@gmail.com>
2023-12-18 21:59:44 +0100

    ga removal

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,23 +1,6 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import Script from "next/script";
-import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Script
-        src="https://www.googletagmanager.com/gtag/js?id=G-2JNP4M4JHK"
-        strategy="afterInteractive"
-      />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`  window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-            gtag('config', 'G-2JNP4M4JHK');`}
-      </Script>
-      <Component {...pageProps} />
-    </>
-  );
+  return <Component {...pageProps} />;
 }
